7fed78498d0c4e050aa56a42aee6937b6ff7d479
andradezdev <227205599+andradezdev@users.noreply.github.com>
2026-08-03 17:02:43 -0300

    Update hooks.py

--- ifitwala_ed/hooks.py
+++ ifitwala_ed/hooks.py
@@ -1,8 +1,8 @@
 # ifitwala_ed/hooks.py
 from ifitwala_ed.routing.policy import WEBSITE_REDIRECTS, WEBSITE_ROUTE_RULES
 
-app_name = "ifitwala_ed"
-app_title = "Ifitwala"
+app_name = "erpz_ed"
+app_title = "ERPZ Educacional"
 app_publisher = "François de Ryckel"
 app_description = "School management System"
 app_email = "f.deryckel@gmail.com"
@@ -11,7 +11,7 @@ app_license = "MIT"
 # Apps
 # ------------------
 
-required_apps = ["ifitwala_drive"]
+required_apps = ["if_drive-gestao-educacional-2"]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
